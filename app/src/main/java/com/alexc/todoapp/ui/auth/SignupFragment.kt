1168eb07d88f19619880704c9c2552e64af558e2
AlexGermancw <agchicaiza2@espe.edu.ec>
2023-02-20 19:08:01 -0500

    Initial commit

---
+++ app/src/main/java/com/alexc/todoapp/ui/auth/SignupFragment.kt
@@ -0,0 +1,21 @@
+package com.alexc.todoapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alexc.todoapp.R
+
+
+class SignupFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+}
